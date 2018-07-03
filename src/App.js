5b72a146785a635498944dac4470a2c3db3710aa
Kendall <nicho147@gmail.com>
2018-07-03 11:59:38 -0400

    change body to Raleway font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Main from './Main.js'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      	chatty
+      	<Main />
       </div>
     );
   }
