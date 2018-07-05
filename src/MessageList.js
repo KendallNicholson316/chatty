4f9bcc4123b8c2e680a9f06b51c7ae396c49d05d
Kendall <nicho147@gmail.com>
2018-07-05 15:21:43 -0400

    refactor MessageList for looks and room announcement

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,27 +1,44 @@
 import React from 'react'
 
-import Message from './Message.js'
+import Message from './Message'
 
 const MessageList = (props) => {
-	return(
-		<div className = "MessageList" style={styles.MessageList}>
-			{
-				props.messages.map(msg => (
-          			<Message message={msg} key={msg.id} />
-        		))
-			}
-		</div>
-	)
+  return (
+    <div
+      className="MessageList"
+      style={styles.messageList}
+    >
+      <div style={styles.roomAnnouncement}>
+        <h3 style={styles.h3}>
+          #general
+        </h3>
+        <p>This is the very beginning of the #general room.</p>
+      </div>
+
+      {
+        props.messages.map(msg => (
+          <Message message={msg} key={msg.id} />
+        ))
+      }
+    </div>
+  )
 }
 
 const styles = {
-	MessageList: {
-  		backgroundColor: 'rgb(219, 220, 221)',
-  		flex: '1',
-  		paddingBottom: '1rem',
-  		overflowY: 'scroll',
-	}
+  messageList: {
+    backgroundColor: 'white',
+    flex: 1,
+    paddingBottom: '1rem',
+    overflowY: 'scroll',
+  },
 
+  roomAnnouncement: {
+    padding: '2rem 1rem',
+  },
+
+  h3: {
+    fontSize: '1.5rem',
+  },
 }
 
 export default MessageList
