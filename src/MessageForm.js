5f5ac0f0cda21e073e977f6a28e7e35a1e5fd2bb
Kendall <nicho147@gmail.com>
2018-07-05 16:25:06 -0400

    style MessageForm with aphrodite

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {StyleSheet, css} from 'aphrodite'
 
 class MessageForm extends Component{
 	state = {
@@ -17,9 +18,9 @@ class MessageForm extends Component{
 
 	render(){
 		return(
-			<form className="MessageForm" 
+			<form 
+				  className={`MessageForm ${css(styles.MessageForm)}`} 
 				  onSubmit={this.handleSubmit}
-				  style = {styles.MessageForm}
 			>
 				<input
           			autoFocus
@@ -29,43 +30,61 @@ class MessageForm extends Component{
           			placeholder="Type a message..."
 					value={this.state.body}
 					onChange={this.handleChange}
-					style = {styles.input}
+					className={css(styles.Input)}
         		/>
-        		<button type="submit" style={styles.button}>
-          			Send
+        		<button 
+					type="submit" 
+					className={css(styles.Button)}
+				>
+          			<i className="far fa-paper-plane" title="Send"></i>	
         		</button>
 			</form>
 		)
 	}
 }
 
-const styles= {
-	MessageForm: {
-  		backgroundColor: 'white',
-  		height: '3rem',
-  		display: 'flex',
-  		alignItems: 'stretch',
-  		border: '2px solid #999',
-  		borderRadius: '0.5rem',
-  		margin: '0.25rem',
-  		padding: '0',
-	},
-	input: {
-  		flex: '1',
-  		fontSize: '1.2rem',
-  		border: '0',
-	},
-	button: {
-  		fontSize: '1.5rem',
-  		backgroundColor: '#1A8FE3',
-  		color: 'white',
-  		paddingLeft: '1rem',
-  		paddingRight: '1rem',
-  		borderTopRightRadius: '0.5rem',
-  		borderBottomRightRadius: '0.5rem',
-  		border: '1px solid white',
-	},
+const styles = StyleSheet.create({
+  MessageForm: {
+    backgroundColor: 'white',
+    height: '3rem',
+    display: 'flex',
+    alignItems: 'stretch',
+    border: '2px solid #999',
+    borderRadius: '0.5rem',
+    margin: '0.25rem',
+    padding: 0,
+  },
 
-}
+  Icon: {
+    display: 'flex',
+    borderRadius: '0.5rem',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    color: '#ccc',
+    padding: '0 0.5rem',
+    fontSize: '1.2rem',
+  },
+
+  Input: {
+    flex: 1,
+    fontSize: '1.2rem',
+    border: 0,
+	paddingLeft: '2vh',
+    ':focus': {
+      outline: 0,
+    },
+  },
+
+  Button: {
+    fontSize: '1.5rem',
+    backgroundColor: '#1A8FE3',
+    color: 'white',
+    paddingLeft: '1rem',
+    paddingRight: '1rem',
+    borderTopRightRadius: '0.5rem',
+    borderBottomRightRadius: '0.5rem',
+    border: '1px solid white',
+  }
+})
 
 export default MessageForm
